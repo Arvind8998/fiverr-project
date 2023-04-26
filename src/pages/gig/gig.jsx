29fa7a55d310039ea9f682be58f6b2fc958a6b61
Arvind Sangwan <asangwan8998@gmail.com>
2023-04-25 23:50:25 -0400

    Added changes

---
+++ src/pages/gig/gig.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const gig = () => {
+  return (
+    <div>gig</div>
+  )
+}
+
+export default gig
---
+++ src/pages/orders/orders.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./orders.scss";
+
+const orders = () => {
+  return <div>Orders</div>;
+};
+
+export default orders;
